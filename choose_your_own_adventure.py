74cd9caab3a834e6b9c9bc3da428ed19d5641059
SRCinVA <scitarella@hotmail.com>
2021-10-25 20:11:28 -0400

    Finished it
    Relatively simple stuff

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,31 +1,40 @@
 name = input("Type your name: ")
 print("Welcome", name, "to this adventure!")
 
-answer1 = input("You're on a dirt road, it has come to an end, and you can go left or right. Which way? ").lower()
+answer = input("You're on a dirt road, it has come to an end, and you can go left or right. Which way? ").lower()
 
-if answer1 == "left":
-    answer2 = input("You come to a river; you walk around it or swim across it. Tyoe 'walk' or 'swim': ")
+if answer == "left":
+    answer = input("You come to a river; you walk around it or swim across it. Type 'walk' or 'swim': ").lower()
 
-    if answer2 == 'swim':
+    if answer == 'swim':
         print("You swam across and were abducted by aliens.")
 
-    elif answer2 == 'walk':
+    elif answer == 'walk':
         print("You walked across for miles and ran out of supplies, so you lost the game.")
 
     else:
         print("Not a valid answer; you lose!")
 
-elif answer1 == "right":
-    print("You've come to a wobbly bridge; do you want to cross it or go back? Type 'cross' or 'go back': ")
-    if answer2 == 'cross':
-            print("You swam across and were abducted by pirates.")
+elif answer == "right":
+    answer = input("You've come to a wobbly bridge; do you want to cross it or go back? Type 'cross' or 'go back': ").lower()
+    
+    if answer == 'cross':
+            answer = input("You swam across and met a stranger. Will you talk to them? Answer 'yes' or 'no.'").lower()
+            
+            if answer == "yes":
+                print("You talked to the stranger and they gave you the map. You win!!")
+            elif answer == "no":
+                print("You ignored the stranger, offending them, so you lose.")
+            else:
+                print("Not a valid answer; you lose!")
 
-    elif answer2 == 'go back':
+    elif answer == 'go back':
         print("You walked back for miles and ran out of supplies, so you lost the game.")
 
     else:
         print("Not a valid answer; you lose!")
 
-
 else:
     print("Not a valid answer; you lose!")
+
+print("Thank you for playing the game, ", name)
